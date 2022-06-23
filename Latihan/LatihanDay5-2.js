702300803e352a4a79c5db70b6087a604ac9820c
wismoon <w.moon091@gmail.com>
2022-06-24 06:08:10 +0700

    Update 5-2

--- Latihan/LatihanDay5-2.js
+++ Latihan/LatihanDay5-2.js
@@ -1,3 +1,6 @@
 //1. Create a separate countries.js file and store the countries array in to this file, 
 // create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
+import {webTechs} from './src/web_techs.js';
+webTechs = require('./src/web_techs.js');
 
+console.log(webTechs);
